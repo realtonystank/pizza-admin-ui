44b6d66442d2fb8149db28633792f5a7f39199db
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-03-15 19:14:52 +0530

    add router

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div>Hello from Admin dashboard</div>;
-}
-
-export default App;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,5 @@
+function Homepage() {
+  return <div>Hello from Admin dashboard</div>;
+}
+
+export default Homepage;
