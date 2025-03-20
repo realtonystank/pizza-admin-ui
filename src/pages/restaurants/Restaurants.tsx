1677b32b532c7a390c4157e1e244788cba54cd14
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-03-20 23:09:51 +0530

    fetch restaurant data from backend and show in table

--- src/pages/restaurants/Restaurants.tsx
+++ src/pages/restaurants/Restaurants.tsx
@@ -1,21 +1,10 @@
-import { Breadcrumb, Card, Space, Table } from "antd";
-import { RightOutlined } from "@ant-design/icons";
+import { Breadcrumb, Card, Flex, Space, Spin, Table, Typography } from "antd";
+import { LoadingOutlined, RightOutlined } from "@ant-design/icons";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import RestaurantsFilter from "./RestaurantsFilter";
-
-const dataSource = [
-  {
-    id: "1",
-    name: "Mike",
-    address: "10 Downing Street",
-  },
-  {
-    id: "2",
-    name: "John",
-    address: "10 Downing Street",
-  },
-];
+import { useQuery } from "@tanstack/react-query";
+import { getTenants } from "../../http/api";
 
 const columns = [
   {
@@ -36,21 +25,47 @@ const columns = [
 ];
 
 const Restaurants = () => {
+  const {
+    data: restaurants,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["tenants"],
+    queryFn: async () => {
+      const response = await getTenants();
+      console.log(response);
+      return response.data;
+    },
+  });
+
   return (
     <>
       <Space size={"middle"} direction="vertical" style={{ width: "100%" }}>
-        <Breadcrumb
-          separator={<RightOutlined />}
-          items={[
-            { title: <NavLink to="/">Dashboard</NavLink> },
-            { title: "Restaurants" },
-          ]}
-        />
+        <Flex justify="space-between">
+          <Breadcrumb
+            separator={<RightOutlined />}
+            items={[
+              { title: <NavLink to="/">Dashboard</NavLink> },
+              { title: "Restaurants" },
+            ]}
+          />
+          {isLoading && (
+            <Spin
+              indicator={
+                <LoadingOutlined style={{ fontSize: 24 }} spin={true} />
+              }
+            />
+          )}
+          {isError && (
+            <Typography.Text type="danger">{error.message}</Typography.Text>
+          )}
+        </Flex>
         <Card>
           <RestaurantsFilter />
         </Card>
 
-        <Table dataSource={dataSource} columns={columns} rowKey={"id"} />
+        <Table dataSource={restaurants} columns={columns} rowKey={"id"} />
       </Space>
     </>
   );
